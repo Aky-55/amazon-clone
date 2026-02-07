7bf96e320a5db35dd305808cdc26068cb238d4ac
AKyadav <alokyadav2542006@gmail.com>
2026-02-07 19:32:27 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,8 +3,9 @@ import{products, getProduct} from '../../data/products.js';
 import{formatCurrency} from '../utils/money.js';
 import dayjs from 'http://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import{deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-const today=dayjs();
-const deliveryDate=today.add(7,'days');
+import { renderPaymentSummary } from './paymentSummary.js';
+
+
 
 export function renderOrderSummary(){
 
@@ -124,6 +125,7 @@ export function renderOrderSummary(){
                 `.js-cart-item-container-${productId}`
             );
             container.remove(); 
+            renderPaymentSummary();
         });
     });
 
@@ -132,6 +134,7 @@ export function renderOrderSummary(){
             const {productId , deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
