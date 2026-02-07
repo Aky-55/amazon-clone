190d91fa7e729aa66d16ecdc4c3b99958ce0300f
AKyadav <alokyadav2542006@gmail.com>
2026-02-07 19:22:15 +0530

    save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import{cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import{products} from '../../data/products.js';
+import{products, getProduct} from '../../data/products.js';
 import{formatCurrency} from '../utils/money.js';
 import dayjs from 'http://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import{deliveryOptions} from '../../data/deliveryDate.js';
+import{deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 const today=dayjs();
 const deliveryDate=today.add(7,'days');
 
@@ -13,22 +13,12 @@ export function renderOrderSummary(){
     cart.forEach((cartItem)=>{
         const productId=cartItem.productId;
 
-        let matchingProduct;
+        let matchingProduct=getProduct(productId);
 
-        products.forEach((product)=>{
-            if(product.id===productId){
-                matchingProduct=product;
-            }
-        }); 
         const deliveryOptionId=cartItem.deliveryOptionId;
 
-        let deliveryOption;
+        const deliveryOption=getDeliveryOption(deliveryOptionId);
 
-        deliveryOptions.forEach((option)=>{
-            if(option.id === deliveryOptionId){
-                deliveryOption=option;
-            }
-        });
 
         const today=dayjs();
         const deliveryDate =today.add(
